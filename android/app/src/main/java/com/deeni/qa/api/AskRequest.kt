ecc83cf3e61e8f878cbbb3da53f149008bb9483a
Azhan <azhan@example.com>
2025-12-28 17:06:38 +0530

    Simplify Android app: remove Retrofit/Moshi, use basic HttpURLConnection

--- android/app/src/main/java/com/deeni/qa/api/AskRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.deeni.qa.api
-
-data class AskRequest(
-    val question: String,
-    val language: String
-)
--- android/app/src/main/java/com/deeni/qa/api/AskResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.deeni.qa.api
-
-data class AskResponse(
-    val answer: String,
-    val source: String?
-)
